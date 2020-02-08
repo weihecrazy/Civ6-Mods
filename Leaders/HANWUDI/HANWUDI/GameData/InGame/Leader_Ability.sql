1a449055a8537c80881a711ab498f2063fb25bb9
WEIHE <liangweihe0818@163.com>
2020-02-08 15:53:50 +0800

    1.更改武刚车为虎贲军，并修改了相应的文字描述 2.增加了时代画报的图片

--- Leaders/HANWUDI/HANWUDI/GameData/InGame/Leader_Ability.sql
+++ Leaders/HANWUDI/HANWUDI/GameData/InGame/Leader_Ability.sql
@@ -1,16 +1,18 @@
 -- Hanwudi_Leader_Ability
 -- Author: 57371
 -- DateCreated: 1/27/2020 3:16:46 PM
+-- 版本：v1.2
+-- 2020年2月8日 v1.1更新：孝武皇帝能力改为每回合+2大将军点数
 --------------------------------------------------------------
 --领袖特殊能力：孝武皇帝
 INSERT INTO TraitModifiers (TraitType, ModifierId) VALUES 
 ("TRAIT_LEADER_HANWUDI_XIAOWU_EMPEROR", "Xiaowu_Emperor_Trait");
 
 INSERT INTO Modifiers (ModifierId, ModifierType, RunOnce, Permanent, NewOnly, OwnerRequirementSetId, SubjectRequirementSetId) VALUES 
-("Xiaowu_Emperor_Trait", "MODIFIER_PLAYER_UNIT_ADJUST_GREAT_PEOPLE_POINTS_PER_KILL", 0, 0, 0, NULL, NULL);
+("Xiaowu_Emperor_Trait", "MODIFIER_PLAYER_ADJUST_GREAT_PERSON_POINTS", 0, 0, 0, NULL, NULL);
 
 INSERT INTO ModifierArguments (ModifierId, Name, Value) VALUES 
-("Xiaowu_Emperor_Trait", "Amount", 4), 
+("Xiaowu_Emperor_Trait", "Amount", 2), 
 ("Xiaowu_Emperor_Trait", "GreatPersonClassType", "GREAT_PERSON_CLASS_GENERAL");
 
 --领袖历史议程：大汉天威
